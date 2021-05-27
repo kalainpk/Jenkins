929681395a6ba6e2901687a307fb05d79bf7168e
kalainpk <kalainpk@gmail.com>
2021-05-27 13:35:40 +0200

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Main  
+{
+   // main method begins execution of Java application
+   public static void main(String[] args)
+   {//w w w.  j av a 2s.  c o  m
+      System.out.println("Welcome to Java Programming!");
+   } 
+}
